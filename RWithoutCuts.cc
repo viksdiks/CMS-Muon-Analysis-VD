a2a9dcab66571e2da6154df7d108ef55b1b4ccb5
viksdiks <victoriadiksokova@gmail.com>
2026-09-10 17:18:17 +0300

    Rename output file to ExperimentalJetsCuts.root

--- RWithoutCuts.cc
+++ RWithoutCuts.cc
@@ -44,7 +44,7 @@ void RWithoutCuts(){
 
     Float_t Muon_leadingPt = -1.;
 
-    TFile *fout = new TFile("ExperimentalJets1.root", "RECREATE");
+    TFile *fout = new TFile("ExperimentalJetsCuts.root", "RECREATE");
 
     t1->SetBranchStatus("*", 0);
     
